1b6d1476b0aa3378b58b80317c92d9e64a37129b
Akash tesla <68317027+akashktesla@users.noreply.github.com>
2022-02-17 23:09:36 +0530

    :)

--- demon-brain/logical&reason_unit.py
+++ demon-brain/logical&reason_unit.py
@@ -21,7 +21,7 @@ class arb:
         self.b = b
 
 
-    class unit:
+class unit:
         def __init__(self,value, unit):
             self.value = value
             self.unit = unit
@@ -34,9 +34,34 @@ def arb_str(aki):
     if str(type(aki)) == "<class 'str'>":
         return str(aki)
 
+
+def store_arb(data):
+    with open("C:\\Users\\akash\PycharmProjects\\Demon's paradise\\demon-brain\\jsonFiles\\memory_arb.json") as f:
+        arb_data = json.load(f)
+        arb_data.append(data)
+    with open("C:\\Users\\akash\PycharmProjects\\Demon's paradise\\demon-brain\\jsonFiles\\memory_arb.json",'w') as w:
+        json.dump(arb_data,w)
+
+
 def str_arb(asi):
-    print(asi)
+    asi = asi.replace(' ','')
+    asi = asi.replace('&a:', ' &a: ')
+    asi = asi.replace('&r:', ' &r: ')
+    asi = asi.replace('&b:', ' &b: ')
+    asi = asi.split()
+    str_arb_sup(asi)
 
 
 
+def str_arb_sup(lsi):
+    for i,item in enumerate(lsi):
 
+        print(item)
+
+def is_arb(str):
+    return "&a:" in str and "&r:" in str and "&b:" in str
+
+if __name__ == "__main__":
+    a = arb(arb('akash','love','saka'),'son','dio')
+    strr = arb_str(a)
+    str_arb(strr)
