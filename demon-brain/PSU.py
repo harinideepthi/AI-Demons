4dbdc474e089960005f140ff6091c4f477b5340a
Akash tesla <68317027+akashktesla@users.noreply.github.com>
2022-03-07 20:28:56 +0530

    :)

--- demon-brain/PSU.py
+++ demon-brain/PSU.py
@@ -4,3 +4,17 @@ def solve(qn):
     print(qn)
 
     pass
+
+def InfoTranslate(sen):
+    """
+
+    param: sen(str)
+    description: unravels a sentence and extracts all information possible/given and returns as a list of data
+    returns: data(list)
+
+    """
+
+
+    data = 0
+    pass
+    return data
